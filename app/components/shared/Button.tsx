d1c9e687a9ece23813737abe8fc5f77efabc8ff7
George Mushore <gmu003@gmail.com>
2024-11-06 00:43:12 +0100

    added icons,assets and tailwind custom colors

--- app/components/shared/Button.tsx
+++ app/components/shared/Button.tsx
@@ -18,16 +18,16 @@ export const Button = ({
   ...props
 }: ButtonProps) => {
   const variantClasses = {
-    primary: 'bg-gray-900 hover:bg-gray-300 text-white',
-    secondary: 'bg-stone-100 hover:bg-white text-black border border-gray-900',
-    tertiary: 'bg-none text-gray-500 hover:text-gray-900 fill-gray-500 hover:fill-gray-900',
-    destroy: 'bg-red-500 hover:bg-red-200 text-white',
+    primary: 'bg-grey-900 hover:bg-grey-300 text-white',
+    secondary: 'bg-beige-100 hover:bg-white text-black border border-grey-900',
+    tertiary: 'bg-none text-grey-500 hover:text-grey-900 fill-grey-500 hover:fill-grey-900',
+    destroy: 'bg-red hover:bg-red-200 text-white',
   };
 
   return (
     <button
       type="button"
-      className={`rounded py-2 px-4 ${variantClasses[variant]} display-flex items-center justify-center`}
+      className={`rounded-lg py-2 px-4 ${variantClasses[variant]} display-flex items-center justify-center`}
       {...props}
     >
       <span className="flex items-center justify-center">
