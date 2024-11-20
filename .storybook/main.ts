c5074b5734cae0a475e0ce669583007082175436
George Mushore <gmu003@gmail.com>
2024-11-20 11:30:26 +0100

    chore: update Storybook configuration; remove unused Button and BaseListItem stories; add new Pots route; update Tailwind CSS color scheme; upgrade dependencies

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,6 +5,7 @@ const config: StorybookConfig = {
     "../stories/**/*.mdx",
     "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
     "../app/components/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../app/features/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
     "@storybook/addon-onboarding",
---
+++ app/routes/pots.tsx
@@ -0,0 +1,10 @@
+
+const Index = () => {
+  return (
+    <div>
+      <h1>Hello, Pots!</h1>
+    </div>
+  );
+};
+
+export default Index;
