f419f3f1f8fc9c8ba01ebc7ffbda1163c266cfae
George Mushore <gmu003@gmail.com>
2024-11-20 11:27:56 +0100

    feat: create new route components for budgets, recurring bills, and transactions; update index route

--- app/routes/budgets.tsx
+++ app/routes/budgets.tsx
@@ -2,7 +2,7 @@
 const Index = () => {
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <h1>Hello, Budgets!</h1>
     </div>
   );
 };
--- app/routes/recurring_bills.tsx
+++ app/routes/recurring_bills.tsx
@@ -2,7 +2,7 @@
 const Index = () => {
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <h1>Hello, Recurringbills!</h1>
     </div>
   );
 };
