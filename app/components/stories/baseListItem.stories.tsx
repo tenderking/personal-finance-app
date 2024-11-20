deb1bbc80f04e3b646cb2f04f5e996d3734cdee8
George Mushore <gmu003@gmail.com>
2024-11-20 11:30:43 +0100

    feat: add Storybook stories for BaseListItem and Button components

---
+++ app/components/stories/baseListItem.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { BaseListItem } from '~/components/shared/BaseListItem';
+
+const meta: Meta<typeof BaseListItem> = {
+  title: 'shared/BaseListItem',
+  component: BaseListItem,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof BaseListItem>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    title: 'Base List Item',
+    image: 'public/avatars/bytewise.jpg',
+  },
+};
+
+export const InList: Story = {
+  render: (args) => (
+    <ul>
+      <li><BaseListItem {...args} /></li>
+      <li><BaseListItem {...args} title="Another List Item" subtitle='hello' /></li>
+      <li><BaseListItem {...args} title="yet Another List Item" /></li>
+    </ul>
+  ),
+  args: {
+    title: 'Base List Item',
+    image: 'public/avatars/bytewise.jpg',
+  },
+};
